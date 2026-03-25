7501fa7a04c12b4f6589a806ca1583930758983d
henriqueramoss-dev <henrigonramos@gmail.com>
2026-03-25 11:10:56 -0300

    Delete tasks.routes.js

--- tasks.routes.js
+++
@@ -1,9 +0,0 @@
-import {Router} from "express";
-import { getTasks, listTasks } from "../controllers/tasks.controller.js";
-
-const router = Router()
-
-router.get("/", listTasks)
-router.get("/:id", getTasks)
-
-export default router
