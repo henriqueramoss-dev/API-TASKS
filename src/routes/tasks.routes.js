c545e842192b04f617fb658b0559aef6d2425121
henriqueramoss-dev <henrigonramos@gmail.com>
2026-03-26 11:24:06 -0300

    Add files via upload

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,9 +1,11 @@
 import {Router} from "express";
-import { getTasks, listTasks } from "../controllers/tasks.controller.js";
+import { createTasks, deleteTasks, getTasks, listTasks, updateTasks } from "../controllers/tasks.controller.js";
 
 const router = Router()
 
 router.get("/", listTasks)
 router.get("/:id", getTasks)
-
+router.post("/", createTasks)
+router.put("/:id", updateTasks)
+router.delete("/", deleteTasks)
 export default router
